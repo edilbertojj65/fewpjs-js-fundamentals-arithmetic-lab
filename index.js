9d06b7c9a17c0ff8a19dc7fd7243decf4c2a6bc0
edilbertojj65 <edilbertojj65@gmail.com>
2021-03-31 23:10:56 -0400

    completed assigment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+  const num1 = 31;
+  const num2 = 2 ;
+
+ const multiply = Number.parseInt(num1,10) * Number.parseInt(num2,10);
+ console.log(multiply);
+ 
+
+
+ let random = (Math.ceil(Math.random() * 10));
+ Number.isInteger(random);
+
+  const num3 = 46;
+  const num4 = 6;
+  let mod = num3 % num4 ;
+ 
+  let max = Math.max(10, 20);
+   
+
+  
+ 
